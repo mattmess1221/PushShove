36707757662f97eac88138390d476c8c45f84f35
Matthew Messinger <mattmess1221@gmail.com>
2015-06-12 12:40:54 -0400

    Add landing/stomping on other entities. When stomping, all fall damage x3 is transfered to the other entity.

--- src/main/java/mnm/mods/pushshove/PushShove.java
+++ src/main/java/mnm/mods/pushshove/PushShove.java
@@ -3,10 +3,13 @@ package mnm.mods.pushshove;
 import java.util.List;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -26,17 +29,36 @@ public class PushShove {
 
         if (!entity.isDead) {
 
-            final double radius = 0.20000000298023224D;
-            AxisAlignedBB bounds = entity.getEntityBoundingBox().expand(radius, 0, radius);
-            List<Entity> list = entity.worldObj.getEntitiesWithinAABBExcludingEntity(entity, bounds);
+            List<Entity> list = getCollidingEntities(entity);
 
-            if (list != null) {
-                for (Entity entity1 : list) {
-                    if (!entity1.isDead && entity1 instanceof EntityPlayer) {
-                        entity.applyEntityCollision(entity1);
-                    }
+            for (Entity entity1 : list) {
+                if (!entity1.isDead && entity1 instanceof EntityPlayer) {
+                    entity.applyEntityCollision(entity1);
                 }
             }
+
         }
+    }
+
+    @SubscribeEvent
+    public void onFall(LivingHurtEvent hurt) {
+        if (hurt.source == DamageSource.fall) {
+            List<Entity> list = getCollidingEntities(hurt.entity);
+            boolean cancel = false;
+            for (Entity entity : list) {
+                if (entity instanceof EntityPlayer || entity instanceof EntityLivingBase) {
+                    cancel = true;
+                    entity.attackEntityFrom(DamageSource.causeMobDamage(hurt.entityLiving), hurt.ammount * 3);
+                }
+
+                hurt.setCanceled(cancel);
+            }
+        }
+    }
+
+    private List<Entity> getCollidingEntities(Entity entity) {
+        final double radius = 0.20000000298023224D;
+        AxisAlignedBB bounds = entity.getEntityBoundingBox().expand(radius, 0, radius);
+        return entity.worldObj.getEntitiesWithinAABBExcludingEntity(entity, bounds);
     }
 }
